562d564b1832634e2dfd83a63901f521cb08ad02
tanhangbo <tan>
2016-03-14 21:29:20 +0800

    function works

--- Markdown.Export/Transform.py
+++ Markdown.Export/Transform.py
@@ -1,8 +1,38 @@
+# -*- coding: utf-8 -*-
 
 import os
 
-print "Hello, World!" 
-os.system("pause")
+process_dir = "D:\\markdown_export\\"
+
+def my_process(line):
+    if line.startswith("![]("):
+        start_pos = line.index("/")
+        line = "![](" + "image" + line[start_pos:]
+    return line;
+
+
+r_file = open(process_dir + "temp.txt", "r")
+w_file = open(process_dir + "1.md", "wb")
+
+
+
+while 1:
+    line = r_file.readline()
+    if not line:
+        break
+    w_file.write(my_process(line))
+
+w_file.close()
+r_file.close()
+	
+os.rename(process_dir + "temp_files", process_dir + "image")
+os.remove(process_dir + "temp.txt");
+os.remove(process_dir + "temp.html");
+os.remove(process_dir + "Transform.py");
+
+
+
+#os.system("pause")
 
 
 
